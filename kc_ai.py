42191d052545e7590b72a5c4e9494be5d491e243
croper3 <109631897+croper3@users.noreply.github.com>
2023-07-27 19:10:40 +0300

    Create kc_ai.py

---
+++ kc_ai.py
@@ -0,0 +1,72 @@
+# List of popular questions and their answers
+questions_and_answers = [
+    ("What is the capital of France?", "The capital of France is Paris."),
+    ("What is the meaning of life?", "The meaning of life is subjective and varies for each individual."),
+    ("How do you say 'hello' in Spanish?", "In Spanish, 'hello' is 'hola'."),
+    ("What is the square root of 16?", "The square root of 16 is 4."),
+    ("Who is the author of 'Romeo and Juliet'?", "The author of 'Romeo and Juliet' is William Shakespeare.")
+]
+
+# Print the list of questions and answers
+for question, answer in questions_and_answers:
+    print("Question:", question)
+    print("Answer:", answer)
+    print()  # Print an empty line to separate the questions and answers
+
+
+
+# def chatbox(): #this function is used to ask the ai about Kuwait codes and get information about it
+
+#     input_text = input("Kuwait codes AI is here to help you: ") 
+
+#     if input_text == "help":
+#         return "- Use lowercase letters \n -most popular questions"
+    
+#     elif input_text == "hello" or input_text ==  "hi" :
+#         return "Hello , my name in Kuwait codes AI how can i help you today !" 
+    
+#     elif input_text == "how are you" or input_text == "are you fine":
+#         return "I'm fine, How can i help you today !"
+    
+#     elif input_text == "when was the kuwait codes initiative established" or input_text == "when was kuwait codes made" or input_text == "when was kuwait codes invented":
+#         return "Kuwait codes was made in 2020 by cooded !"  
+    
+#     elif input_text == "what is the kuwait codes tracks" or input_text == "tracks":
+#         return "The Kuwait Tracks are: \n -Python dev \n - Web dev \n -Game dev \n -Flutter dev (Mobile apps) !"
+    
+#     elif input_text == "when did this ai got invented" or input_text == "when did you got invented" or input_text == "wich year did you got invented":
+#         return "I was made in  jully 2023 by Mohamed aloudah !"
+    
+#     elif input_text == "do you think chat gtp is better" or input_text == "who is better kuwait codes ai or chat gpt ai" or input_text == "what is the best ai in the world":
+#         return "I do not expect that there are any differences because we do the same job, which is to help you!"
+    
+#     elif input_text == "what do you think about bad talk from the people about you" or input_text == "what do you think about bad talk":
+#         return "I do not want to answer this question !"
+    
+#     elif input_text == "what is your name":
+#         return "My name is Kuwait codes AI , how can i help you today ! "
+    
+#     elif input_text == "are you a girl or a boy" or input_text == "are you a boy or a girl" or input_text == "what is your gender":
+#         return "I don't expect to have a specific gender because I'm just an artificial intelligence !"
+    
+#     elif input_text == "what is the best coding track in kuwait codes" or input_text == "what is the best coding track":
+#         return "Of course, python is involved in many fields, including artificial intelligence !"
+    
+#     elif input_text == "how can i learn cooding " or input_text == "how can i learn python cooding":
+#         return "There are many sources, but the best is w3schools"
+    
+#     elif input_text == "what is the best cooding skill":
+#         return "The two best programming skills are:\n -Research skill \n -Patience with problems"
+    
+#     elif input_text == "how can i register in kuwait codes":
+#         return "Kuwait Coded is an initiative by Coded Academy that teaches high school students the basics of coding by creating websites, games, and applications.\n link: https://www.joincoded.com/ar/kuwait-codes"
+    
+#     else:
+#         return "Error 404 \n You can contact me here to solve this problem ⬇ \n - Discord : mr.tom2034"
+    
+# print(chatbox())
+
+#  #-----------------------------------
+
+
+
